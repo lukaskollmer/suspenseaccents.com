214974eaba75d4b15a019c9d05ae01838ec1200d
Lukas Kollmer <lukas.kollmer@gmail.com>
2018-05-02 17:20:31 +0200

    bug fixes and performance improvements

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,31 @@
+const sounds = [undefined]
 
+const createButton = index => {
+  const wrapper = document.createElement('div')
+  const button  = document.createElement('a')
+  const text    = document.createElement('p')
+
+  wrapper.appendChild(button)
+  button.appendChild(text)
+
+  wrapper.classList.add('three', 'columns')
+  button.classList.add("button")
+  button.index = index
+  text.innerHTML = `Play #${index}`
+  
+  button.onclick = sender => {
+    const sound = sounds[sender.path[1].index]
+    sound.play()
+  }
+
+  return wrapper
+}
+
+Array(8).fill().forEach((_, idx) => {
+  idx += 1
+  sounds.push(new Audio(`/static/${idx}.mp3`))
+
+  const rowNumber = (idx-1) / 4 < 1 ? 0 : 1
+
+  document.getElementById(`row-${rowNumber}`).appendChild(createButton(idx))
+})
